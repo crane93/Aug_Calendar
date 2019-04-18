5d8734de5a50884fd179ed3f43e5df62f0c360b4
crane93 <youl9094@gmail.com>
2019-04-18 10:09:51 +0900

    1을 눌렀을 때 배경색이 빨개짐

--- use_JS/scripts.js
+++ use_JS/scripts.js
@@ -1,6 +1,18 @@
 window.onload = function ()
 {
-    document.getElementById('header').onclick = function () {
-        this.style.color = 'red';
+    document.getElementById('days').onclick = function () {
+        this.style.backgroundColor = 'red'
+    }
+}
+
+  //다시 클릭하면 원상태로 돌아오는 코드 추가
+
+function basicColor(){
+    var day = document.getElementById("days");
+
+    if(day.style.backgroundColor === "red"){
+        day.onclick = function(){
+            this.style.backgroundColor = "blue";
+        }
     }
 }
