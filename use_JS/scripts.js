5d6d240ae9acfef0a068a40ed5abb8f2bd455b77
cheekyorkind <cheekorkind@gmail.com>
2019-04-22 22:36:08 +0900

    done

--- use_JS/scripts.js
+++ use_JS/scripts.js
@@ -9,44 +9,27 @@
  *window.onload = function()* 을 쓰고 그 안에 실행하려는 함수를 적어준다
  그러면 index.html에 script를 안적어도 실행이 된다
 */
-window.onload = function ()
-{
-    changeBackColor();
-    inputName();
-    basicColor();
+window.onload = function () {
+  changeBackColor();
 }
+
 
 //html에 있는 스크립트 가져옴
-function changeBackColor(){
-    var containers = document.getElementsByClassName("changeBackground");
-    for(var i=0; i<containers.length;i++){
-        // var ths = containers[i].querySelectorAll("th");
-        containers[i].addEventListener('click', function(event){
-            event.target.style.backgroundColor = 'red';
-            //* event.target: 이벤트가 시작된 DOM 요소 
-            // https://recoveryman.tistory.com/82
-        });
-    }    
-}
+function changeBackColor() {
+  var containers = document.getElementsByClassName("changeBackground");
+  for (var i = 0; i < containers.length; i++) {
+    var ths = containers[i].querySelectorAll("th");
 
-function inputName(){
-    const para = document.querySelector('h1');    //h1을 선택한다
-    para.addEventListener('click', updateName);   //addEvent리스너를 붙여서 h1이 선택되었을 때 업데이트네임이 실행되게 한다
-    function updateName() {
-      let name = prompt('Enter a new name');
-      para.textContent = '8월이 생일인 ' + name;
+    var thIndex = 0;
+    while (thIndex < ths.length) {
+      ths[thIndex].addEventListener('click', function (event) {
+        if (event.target.style.backgroundColor == 'red') {
+          event.target.style.backgroundColor = '';
+        } else {
+          event.target.style.backgroundColor = 'red';
+        }
+      });
+      thIndex++;
     }
-}
-
-  //다시 클릭하면 원상태로 돌아오는 코드 
-  function basicColor(){
-    let para = document.querySelector('th');
-    let compStyles = window.getComputedStyle(para);
-    var pickedcell = document.getElementsByTagName("th");
-    //var backRed = document.getElementsByTagName("th").style.backgroundColor;
-    if(compStyles.getPropertyValue('backgroundColor') == 'red'){
-        pickedcell.addEventListener('click', function(event){
-            event.target.style.backgroundColor="blue";
-        });
-    }
+  }
 }
